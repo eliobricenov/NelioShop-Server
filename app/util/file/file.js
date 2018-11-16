473e0e0cbe73a1ad6773c9ffa08f40620eee15e5
Elio Briceño <eliobricenov@gmail.com>
2018-11-16 00:42:34 -0400

    adde file manipulation

---
+++ app/util/file/file.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images/uploads')
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+const imageFilter = function (req, file, cb) {
+    // accept image only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+const upload = multer({storage: storage, fileFilter: imageFilter});
+
+module.exports = {upload};
+
